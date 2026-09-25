96b93ca94423fd345b398e0228879fe3fce743ae
Lucas Ferreira <lucasfe@gmail.com>
2026-09-25 01:24:01 -0300

    snap 0.4.2

--- Casks/snap.rb
+++ Casks/snap.rb
@@ -1,8 +1,8 @@
 cask "snap" do
-  version "0.4.1"
-  sha256 "8d9a1e391fb0e2af3383252b9d575df9f323dbf076236dd313f83a91983a9315"
+  version "0.4.2"
+  sha256 "e45dabd072456004e2d21c1201b3a8f0c877b045b2fd6820a4b916ae2977633f"
 
-  url "https://github.com/lucasfe/homebrew-snap/releases/download/v0.4.1/Snap-0.4.1.dmg"
+  url "https://github.com/lucasfe/homebrew-snap/releases/download/v0.4.2/Snap-0.4.2.dmg"
   name "Snap"
   desc "Menu bar screenshot tool with annotation and issue filing"
   homepage "https://github.com/lucasfe/homebrew-snap"
